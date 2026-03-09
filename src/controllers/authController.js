69af1e0a3c313edfaa380d9d862c170c35e9c5c6
Ishraq Syed <Ishraq.syed0407@gmail.com>
2026-03-09 14:23:09 +0530

    allow fior now

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,6 +8,7 @@ const cookieOptions = {
   ),
   httpOnly: true,
   sameSite: "None",
+  secure: false,
   // sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
   // secure: process.env.NODE_ENV === "production",
 };
